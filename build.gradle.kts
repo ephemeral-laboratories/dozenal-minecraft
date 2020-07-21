7973be1b9340c188b5e38fd8065b784730a99f17
Trejkaz (pen name) <trejkaz@trypticon.org>
2020-07-22 05:52:59 +1000

    Initial build for modpack, reproducibly builds a zip for the first release

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,10 @@
 version = "1.0"
 group = "garden.ephemeral.dozenal"
 
+var minecraftVersion = "1.15.2"
+var forgeVersion = "31.2.22-1.15.x-i18n-fixes"
+var shortForgeVersion = "31.2-i18n-fixes"
+
 apply(plugin = "base")
 
 tasks.register<Zip>("resourcePack") {
@@ -9,6 +13,36 @@ tasks.register<Zip>("resourcePack") {
     destinationDirectory.set(file("${buildDir}/dist"))
     from("resource-pack")
 }
+
+tasks.register<Zip>("modpack") {
+    archiveFileName.set("dozenal-modpack-${project.version}.zip")
+    destinationDirectory.set(file("${buildDir}/dist"))
+    includeEmptyDirs = true
+
+    into("/") {
+        from("modpack/resources")
+        rename("dot-minecraft/(.*$)", ".minecraft/$1")
+        exclude("dot-minecraft/")
+    }
+
+    into(".minecraft") {
+        from("modpack/resources/dot-minecraft")
+    }
+
+    into(".minecraft/resourcepacks/dozenal-${version}") {
+        from("resource-pack")
+    }
+
+    into("/") {
+        from("modpack/templates")
+        filter { line: String -> line
+            .replace("@MINECRAFT_VERSION@", minecraftVersion)
+            .replace("@FORGE_VERSION@", forgeVersion)
+            .replace("@SHORT_FORGE_VERSION@", shortForgeVersion)
+        }
+    }
+}
+
 tasks.named("assemble") {
-    dependsOn("resourcePack")
+    dependsOn("resourcePack", "modpack")
 }
